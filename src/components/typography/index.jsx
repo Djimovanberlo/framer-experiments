4192539b82431973955f26108d6540e03e850d9a
Djimo van Berlo <djimovanberlo@gmail.com>
2022-11-23 17:04:10 +0100

    init commit

---
+++ src/components/typography/index.jsx
@@ -0,0 +1,3 @@
+export const P = ({ children, className = '' }) => <p className={`p ${className}`}>{children}</p>
+
+export const H1 = ({ children, className = '' }) => <h1 className={`h1 ${className}`}>{children}</h1>
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import { H1, P } from '../components/typography'
+
+const Home = () => {
+  return (
+    <main className='home'>
+      <H1>Header</H1>
+      <P>Paragraph</P>
+    </main>
+  )
+}
+
+export default Home
